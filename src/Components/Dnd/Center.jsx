c400d06b143d0277999dd2bf805d65f3b62f7688
hipppoSHL <shjh1114@icloud.com>
2019-01-28 16:54:15 +0900

    division not done...

--- src/Components/Dnd/Center.jsx
+++ src/Components/Dnd/Center.jsx
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import Draggable from "./Draggable";
 import DroppableBox from "./DroppableBox";
 
-import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-import ButtonModify from '../../Components/RightSidebar/ButtonModify';
-import ButtonForm from '../../Components/RightSidebar/ButtonForm';
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 import "bootstrap/dist/css/bootstrap.css";
 
-let box1=[]
-let box2=[]
-let box3=[]
+import DivisionContent from "./divisionContent";
+
+let box1 = [];
+let box2 = [];
+let box3 = [];
+let box4 = [];
+let box5 = [];
+let box6 = [];
+let box7 = [];
+let box8 = [];
+let box9 = [];
 
 const Wrapper = styled.div`
   width: 100%;
@@ -19,25 +29,26 @@ const Wrapper = styled.div`
   justify-content: center;
 `;
 
-const defaultBtnStyle = {
-  asset: "button",
-  orientation: " vertical",
-  div: 1,
-  argv: {
-    class: "btn btn-primary",
-    href: "",
-    string: "button"
+const styles = {
+  card: {
+    minWidth: 275
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
   }
 };
 
-let btnIndx = 7;
-let navbarindx = 1;
-let progressindx = 1;
-let listindx = 1;
 let justCheck = true;
 
 class Center extends Component {
-
   constructor() {
     super();
     this.displayData = [];
@@ -46,120 +57,41 @@ class Center extends Component {
     textValue: "initial value",
     showdata: this.displayData,
     checkAdd: true,
-    boxes : [],
-    elements : []
+    selectedBox: "box1",
   };
 
-  // elementMove = (element, id) => {
-  //   console.log("finally came to parent component!", element, " id:", id);
+  splitHR = e => {
+    console.log("split HR!");
+  }
 
-  //   let project = this.state.elements.find(p => {
-  //     return p.argv.string === element.argv.string;
-  //   });
-  //   project.div = id;
-  //   console.log("this.state2.element", this.state.elements);
-  // };
-
-  changeText = e => {
+  divSelect = (e) => {
+    const selectedbox = e.target.boxId;
     this.setState({
-      textValue: e.target.value
+      selectedBox: selectedbox
     });
-    console.log(this.state.textValue);
-  };
+    console.log(this.state.selectedBox);
+  }
 
-  static getDerivedStateFromProps(nextProps,prevState){
-    if (nextProps.elements !== prevState.elements ) {
-      console.log("props >>>> ",nextProps.elements);
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.elements !== prevState.elements) {
+      console.log("props >>>> ", nextProps.elements);
       justCheck = true;
-      box1=[]
-      box2=[]
-      box3=[]
+      box1 = [];
+      box2 = [];
+      box3 = [];
       return { elements: nextProps.elements };
     }
     return null;
   }
 
-  // navbar 추가하는 메소드
-  addNavbar = e => {
-    e.preventDefault();
-    let pushingElement = {
-      id: "navbar" + navbarindx++,
-      asset: "navbar",
-      orientation: " vertical",
-      div: "box1",
-      argv: {
-          title:"portals",
-          menu:[
-              "google",
-              "naver",
-              "daum"
-          ],
-          link:[
-              "https://google.com",
-              "https://naver.com",
-              "https://daum.net"
-          ]
-      }
-    };
-    box1.push(pushingElement);
-    this.state2.elements.push(pushingElement);
-    this.setState({
-      checkAdd: !this.state.checkAdd
-    });
-  }
-
-  // 프로그레스 추가하는 메소드
-  addProgress = e => {
-    e.preventDefault();
-    let pushingElement = {
-      id: "progress" + progressindx++,
-      asset: "progress",
-      orientation: " vertical",
-      div: "box1",
-      argv: {
-          percentage: "30"
-      }
-    };
-    box1.push(pushingElement);
-    this.state2.elements.push(pushingElement);
-    this.setState({
-      checkAdd: !this.state.checkAdd
-    });
-  }
-
-  // list 추가하는 메소드
-  addList = e => {
-    e.preventDefault();
-    let pushingElement = {
-      id: "list" + listindx++,
-      asset: "list",
-      orientation: " vertical",
-      div: 1,
-      argv: {
-          count: 3,
-          menu:[
-              "google",
-              "naver",
-              "daum"
-          ],
-          link:[
-              "https://google.com",
-              "https://naver.com",
-              "https://daum.net"
-          ]
-      }
-    };
-    box1.push(pushingElement);
-    this.state.elements.push(pushingElement);
-    this.setState({
-      checkAdd: !this.state.checkAdd
-    });
-  }
-
   render() {
-    const {elementMove} =this.props;
+    const { classes, elementMove } = this.props;
     console.log("rendered!");
-    
+
+    let division = (
+      <DivisionContent />
+    );
+
     if (justCheck === true) {
       this.state.elements.map(element => {
         if (element.div === "box1") {
@@ -176,11 +108,12 @@ class Center extends Component {
 
     return (
       <div>
-        <Wrapper>
+        <div style={{ display: "flex", justifyContent: "center" }}>
           <DroppableBox
             boxId="box1"
             elements={box1}
             whenSomethingCame={elementMove}
+            onClick={this.divSelect}
           />
           <DroppableBox
             boxId="box2"
@@ -192,38 +125,42 @@ class Center extends Component {
             elements={box3}
             whenSomethingCame={elementMove}
           />
-        </Wrapper>
-        <div>
-          <TextField
-            id="standard-full-width"
-            style={{ margin: 8 }}
-            placeholder="Button input"
-            fullWidth
-            margin="normal"
-            value={this.state.textValue}
-            onChange={this.changeText}
-            InputLabelProps={{
-              shrink: true
-            }}
-          />
         </div>
-        <div>
-          {/* <Button variant="contained" color="primary" onClick={this.addButton}>
-            Add a button!
-          </Button> */}
-          <Button variant="contained" color="primary" onClick={this.addNavbar}>
-            Add a navbar! 잘 안돼...
-          </Button>
-          <Button variant="contained" color="primary" onClick={this.addProgress}>
-            Add a Progress!
-          </Button>
-          <Button variant="contained" color="primary" onClick={this.addList}>
-            Add a List!
-          </Button>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          { division }
+          {/* <div style={{border: "1px solid black"}}>dkjfsfj</div> */}
         </div>
+
+        {/* 서버에서 받아서 iframe 에다가 코드 넣어줘야함 */}
+        {/* <iframe
+          width="800px"
+          height="200px"
+          srcDoc="<html><body>Hello, <b>world</b>.</body></html>"
+        /> */}
+
+        {/* division 나눌 수 있게 버튼 추가한 카드 */}
+        <Card className={classes.card}>
+          <CardContent>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              onClick={this.splitHR}
+            >
+              Split horizontally
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              Split vertically
+            </Button>
+          </CardContent>
+        </Card>
       </div>
     );
   }
 }
 
-export default Center;
+export default withStyles(styles)(Center);
---
+++ src/Components/Dnd/divisionContent.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class DivisionContent extends Component {
+  render() {
+    return <div style={{ border: "1px solid black" }}>dkjfsfj</div>;
+  }
+}
+
+export default DivisionContent;
